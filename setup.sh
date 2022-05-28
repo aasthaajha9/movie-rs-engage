a37da9806517e2d1e462739884785fc0f3aac7be
aasthaajha9 <106265134+aasthaajha9@users.noreply.github.com>
2022-05-29 00:29:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "aasthaajhawork@gmail.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
